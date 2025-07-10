c162b9ac2bd301ac81f997352651383076315349
nt636@tux.cci.drexel.edu <khanhnguyentrinhthi@gmail.com>
2025-07-10 11:30:22 -0400

    Initial Commit

---
+++ scroll.js
@@ -0,0 +1,24 @@
+
+function updateScrollProgress() {
+            const scrollProgress = document.querySelector('.dynamic-accent');
+            const scrollTop = document.documentElement.scrollTop;
+            const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const scrollPercentage = (scrollTop / scrollHeight) * 100;
+            
+            scrollProgress.style.height = scrollPercentage + '%';
+}
+        
+        window.addEventListener('scroll', updateScrollProgress);
+        
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    target.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            });
+});
